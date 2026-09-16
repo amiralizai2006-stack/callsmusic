320eecae1b5e179d7edaae0d58e7acfa2704b25f
amiralizai2006-stack <amiralizai2006@gmail.com>
2026-09-16 16:30:28 +0430

    Add OWNER_ID, SUPPORT_USERNAME, REQUIRED_CHANNEL, STRING_SESSION, SUBSCRIPTIONS_DB to config; default-safe parsing

--- callsmusic/config.py
+++ callsmusic/config.py
@@ -5,8 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', ''))
+API_ID = int(getenv('API_ID', '0') or 0)
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '7'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ !').split())
-SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
+SUDO_USERS = list(map(int, filter(None, getenv('SUDO_USERS', '').split())))
+
+# Optional session string for userbot mode (if used)
+STRING_SESSION = getenv('STRING_SESSION')
+
+# Owner/support/config
+OWNER_ID = int(getenv('OWNER_ID', '0') or 0)
+SUPPORT_USERNAME = getenv('SUPPORT_USERNAME')
+REQUIRED_CHANNEL = getenv('REQUIRED_CHANNEL')
+
+# Subscriptions DB path
+SUBSCRIPTIONS_DB = getenv('SUBSCRIPTIONS_DB', 'callsmusic_subscriptions.db')
