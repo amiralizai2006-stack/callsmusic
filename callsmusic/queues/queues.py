975564959e55cbd748f8add27378da5911d79bc3
amiralizai2006-stack <amiralizai2006@gmail.com>
2026-09-16 16:27:30 +0430

    Add list_queue snapshot API and metadata support to queues

--- callsmusic/queues/queues.py
+++ callsmusic/queues/queues.py
@@ -1,13 +1,53 @@
 from asyncio import Queue as _Queue
 from asyncio import QueueEmpty as Empty
-from typing import Dict
+from typing import Dict, List, Any
 
 
 class Queue(_Queue):
-    _queue: list = []
+    def __init__(self):
+        super().__init__()
+        # maintain a snapshot list for read-only views
+        self._items: List[Dict[str, Any]] = []
+
+    async def put(self, item):
+        # item is expected to be a dict
+        self._items.append(item)
+        await super().put(item)
+
+    def get_nowait(self):
+        # pop from snapshot then return from underlying queue
+        try:
+            item = super().get_nowait()
+        except Exception:
+            raise Empty
+        # also remove from snapshot if present
+        if self._items:
+            try:
+                self._items.pop(0)
+            except Exception:
+                pass
+        return item
+
+    def qsize(self):
+        return len(self._items)
+
+    def empty(self):
+        return len(self._items) == 0
+
+    def task_done(self):
+        try:
+            super().task_done()
+        except Exception:
+            pass
 
     def clear(self):
-        self._queue.clear()
+        # clear snapshot and drain underlying queue
+        self._items.clear()
+        try:
+            while True:
+                super().get_nowait()
+        except Exception:
+            pass
 
 
 queues: Dict[int, Queue] = {}
@@ -20,7 +60,7 @@ async def put(chat_id: int, **kwargs) -> int:
     return queues[chat_id].qsize()
 
 
-def get(chat_id: int) -> Dict[str, str]:
+def get(chat_id: int) -> Dict[str, Any]:
     if chat_id in queues:
         try:
             return queues[chat_id].get_nowait()
@@ -37,10 +77,7 @@ def is_empty(chat_id: int) -> bool:
 
 def task_done(chat_id: int):
     if chat_id in queues:
-        try:
-            queues[chat_id].task_done()
-        except ValueError:
-            pass
+        queues[chat_id].task_done()
 
 
 def clear(chat_id: int):
@@ -50,3 +87,10 @@ def clear(chat_id: int):
         else:
             queues[chat_id].clear()
     raise Empty
+
+
+def list_queue(chat_id: int) -> List[Dict[str, Any]]:
+    """Return a snapshot list of queued items for chat_id without removing them."""
+    if chat_id in queues:
+        return list(queues[chat_id]._items)
+    return []
